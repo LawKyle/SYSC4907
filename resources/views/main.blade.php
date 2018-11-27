f1dfa4253be00e2ef26cc21d65c3f15002dcc4f2
Kshamina <kshamina.ghelani@carleton.ca>
2018-11-27 11:00:08 -0500

    update ui

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,71 +16,113 @@
 
     <link href="{{asset('bootstrap-4.1.3-dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{asset('css/sb-admin.css')}}" rel="stylesheet">
 
     <title>Grocery Web App</title>
   </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-2">
-          <div class="sidenav">
-            <h3>Grocery Web App</h3>
-            <a href="#">
-              <div class="row">
-                <div class="col text-center">
-                  <i class="fas fa-user-circle fa-2x"></i>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col text-center">
-                  My Profile
-                </div>
-              </div>
-            </a>
-            <a href="#">
-              <div class="row">
-                <div class="col text-center">
-                  <i class="fas fa-clipboard-list fa-2x"></i>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col text-center">
-                  My Grocery List
-                </div>
-              </div>
-            </a>
-            <a href="#">
-              <div class="row">
-                <div class="col text-center">
-                  <i class="fas fa-shopping-cart fa-2x"></i>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col text-center">
-                  Tagged Products
-                </div>
-              </div>
-            </a>
+  <body id="page-top">
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1" href="index.html">Grocery</a>
+
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Navbar Search -->
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" action="{{action('SearchController@searchBar')}}" method="GET">
+        @csrf
+        <div class="input-group">
+          <input name="query" type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+              <i class="fas fa-search"></i>
+            </button>
           </div>
         </div>
-        <div class="searchForm col-md">
-          <div class="row">
-              <div class="col">
-                  <form class="form-inline" action="{{action('SearchController@searchBar')}}" method="GET">
-                      @csrf
-                      <input id="query" name="query" type="text" class="form-control" placeholder="Search for products ...">
-                      <button id="searchBtn" type="submit" class="btn btn-outline-primary">Search</button>
-                  </form>
-              </div>
+      </form>
+
+      <!-- Navbar -->
+      <ul class="navbar-nav ml-auto ml-md-0">
+        <li class="nav-item dropdown no-arrow mx-1">
+          <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bell fa-fw"></i>
+            <span class="badge badge-danger">9+</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
           </div>
+        </li>
+        <li class="nav-item dropdown no-arrow mx-1">
+          <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-envelope fa-fw"></i>
+            <span class="badge badge-danger">7</span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle fa-fw"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="#">Settings</a>
+            <a class="dropdown-item" href="#">Activity Log</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+          </div>
+        </li>
+      </ul>
+
+    </nav>
+
+    <div id="wrapper">
+    
+      <!-- Sidebar -->
+      <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ action('SearchController@test') }}">
+            <i class="fas fa-shopping-cart"></i>
+            <span>Products</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">
+            <i class="fas fa-user-circle"></i>
+            <span>My Profile</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">
+            <i class="fas fa-clipboard-list"></i>
+            <span>My Grocery List</span>
+          </a>
+        </li>
+      </ul>
+
+      <!-- Sidebar Departments -->
+      <ul id="sidebar-dept" class="sidebar navbar-nav">
+          @foreach(Department::getDepartments() as $dept)
+            <li class="nav-item">
+              <a class="nav-link" href="/department/{{$dept}}">{{ $dept }}</a>
+            </li>
+            @endforeach
+      </ul>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+
+          <!-- Page Content -->
           <div class="row content">
-            <div class="col-md-3">
-                <div class="sidenav-dept">
-                  @foreach(Department::getDepartments() as $dept)
-                    <a href="/department/{{$dept}}"> {{ $dept }} </a><br>
-                  @endforeach
-                </div>
-            </div>
             <div class="col">
                 @foreach ($products as $product)
                     <p>{{ $product->getID() }}</p>
@@ -94,24 +136,64 @@
                     <hr>
                 @endforeach
             </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+        <!--<footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright © Your Website 2018</span>
+            </div>
+          </div>
+        </footer>-->
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="login.html">Logout</a>
           </div>
         </div>
       </div>
-
     </div>
 
+            </div>
+        </div>
+   
+
+    
+
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <!-- JQuery -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/js/mdb.min.js"></script>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script type="text/javascript" src="{{ asset('js/sb-admin.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('js/sb-admin.min.js')}}"></script>
   </body>
 </html>
