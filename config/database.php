45c452832b9916acfb2f7cffcd77bfc347d4ba3f
Kshamina <kshamina.ghelani@carleton.ca>
2019-01-11 16:37:07 -0500

    local db for now

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '192.168.10.10'),
+            'host' => env('DB_HOST', 'lawrynuik.ca'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'TapCheck'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
+            'database' => env('DB_DATABASE', 'FourthYearProject'),
+            'username' => env('DB_USERNAME', 'Kshamina'),
+            'password' => env('DB_PASSWORD', 'KshPass'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
