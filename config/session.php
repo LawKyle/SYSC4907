28e0bfc0ec8aadab1e7ea55ec4339847378f3b13
Kshamina <kshamina.ghelani@carleton.ca>
2019-01-20 19:48:09 -0500

    3

--- config/session.php
+++ config/session.php
@@ -153,7 +153,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'www.grocer-tap.com'),
+    'domain' => '.grocer-tap.com',
 
     /*
     |--------------------------------------------------------------------------
